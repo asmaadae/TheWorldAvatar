8e6144d7a2593699d3ff9b7c9f82169b2fb0b834
nagarajankarthik <nkarthik3092@gmail.com>
2023-11-21 17:11:23 +0800

    1690-add-heat-instantiation-agent: Updated local endpoint in unit test.

--- Agents/HeatInstantiationAgent/agent/instantiate.py
+++ Agents/HeatInstantiationAgent/agent/instantiate.py
@@ -254,7 +254,7 @@ if __name__ == '__main__':
     dirname = os.path.dirname(__file__)
     data_file = os.path.join(dirname, '../data/heat.xlsx')
     df = get_heat_chemicals(data_file)
-    update_endpoint = "http://localhost:8080/blazegraph/namespace/sgbusinessunits/sparql/"
-    update_endpoint = "http://theworldavatar.com/blazegraph/namespace/sgbusinessunits/sparql/"
+    update_endpoint = "http://localhost:48889/blazegraph/namespace/sgbusinessunits/sparql/"
+    # update_endpoint = "http://theworldavatar.com/blazegraph/namespace/sgbusinessunits/sparql/"
     kgclient_heat = KGClient(update_endpoint, update_endpoint)
     instantiate_data(df, kgclient_heat)
